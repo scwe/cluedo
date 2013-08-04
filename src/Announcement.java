3ec7264031c77ea0e0e394c156aac5fe0e9f55a1
Scott <weston.scot@gmail.com>
2013-08-04 22:16:03 +1200

    Work towards getting announcements working

--- src/Announcement.java
+++ src/Announcement.java
@@ -1,14 +1,43 @@
-import java.util.ArrayList;
 
 public class Announcement {
 	
-	private ArrayList<Cardable> cards;
+	private Player p;
+	private Card room;
+	private Card character;
+	private Card weapon;
 	
-	public Announcement(Cardable room, Cardable character, Cardable weapon){
-		this.cards = new ArrayList<Cardable>();
+	public Announcement(Player p, Card room, Card character, Card weapon){
+		this.p = p;
+		this.room = room;
+		this.character = character;
+		this.weapon = weapon;
 	}
 	
 	public boolean isValid(){
-		return false;
+		return room.getCard() instanceof Room && character.getCard() instanceof Character && weapon.getCard() instanceof Weapon;
+	}
+	
+	public Card getRoom() {
+		return room;
+	}
+	
+	public void setRoom(Card room) {
+		this.room = room;
+	}
+
+	public Card getCharacter() {
+		return character;
+	}
+
+	public void setCharacter(Card character) {
+		this.character = character;
+	}
+
+	public Card getWeapon() {
+		return weapon;
+	}
+	
+	public void setWeapon(Card weapon) {
+		this.weapon = weapon;
 	}
 }
