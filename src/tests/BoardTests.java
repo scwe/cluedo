f40797d8519c101c98297afcf2539b2d491d2fe8
Scott <weston.scot@gmail.com>
2013-08-11 21:32:18 +1200

    More unit tests

--- src/tests/BoardTests.java
+++ src/tests/BoardTests.java
@@ -9,173 +9,170 @@ import card.*;
 import java.util.*;
 import java.io.*;
 
-
 public class BoardTests {
 
-    @Test
-    public void boardLoader_test1() {
-        BoardLoader b = new BoardLoader(new File("Map"), 31, 30); 
-        b.getBoard();
-    }
+	@Test
+	public void boardLoader_test1() {
+		BoardLoader b = new BoardLoader(new File("Map"), 31, 30);
+		b.getBoard();
+	}
 
-    @Test
-    public void contextGenerator_test1() {
-        ContextGenerator cg = new ContextGenerator();
-    }
-    
-    @Test
-    public void contextGenerator_test2() {
-        assertEquals(new ContextGenerator().createContext("hall", ' ').getString(), " ");
-        assertEquals(new ContextGenerator().createContext("wall", ' ').getString(), "#");
-        assertEquals(new ContextGenerator().createContext("door-north", ' ').getString(), "^");
-        assertEquals(new ContextGenerator().createContext("room-label", 'h').getString(), "h");
-    }
+	@Test
+	public void contextGenerator_test1() {
+		ContextGenerator cg = new ContextGenerator();
+	}
 
+	@Test
+	public void contextGenerator_test2() {
+		assertEquals(new ContextGenerator().createContext("hall", ' ').getString(), " ");
+		assertEquals(new ContextGenerator().createContext("wall", ' ').getString(), "#");
+		assertEquals(new ContextGenerator().createContext("door-north", ' ').getString(), "^");
+		assertEquals(new ContextGenerator().createContext("room-label", 'h').getString(), "h");
+	}
 
-    @Test
-    public void drawContext_text1(){
-    	DrawContext dw  = new DrawContext("hallway", null);
-    	DrawContext dw2 = new DrawContext("", null);
-    	DrawContext dw3 = new DrawContext(null, null);
-    }
-    
-    @Test
-    public void drawContext_text2(){
-    	DrawContext dw  = new DrawContext("hallway", null);
-    	assertEquals(dw.getString(), "hallway");
-    	assertFalse(new DrawContext("something", null).getString().equals("another thing"));
-    }
-    
-    @Test
-    public void textBoard_test1(){
-    	TextBoard b = new TextBoard();
-    }
+	@Test
+	public void drawContext_text1() {
+		DrawContext dw = new DrawContext("hallway", null);
+		DrawContext dw2 = new DrawContext("", null);
+		DrawContext dw3 = new DrawContext(null, null);
+	}
 
-    @Test
-    public void textBoard_test2(){
-    	TextBoard b = new TextBoard();
-    	
-    	assertEquals(b.getStartLocation(0), new Location(7, 1));
-    	assertEquals(b.getStartLocation(3), new Location(8, 29));
-    	assertEquals(b.getStartLocation(4), new Location(1, 20));
-    	
-    	assertFalse(b.getStartLocation(2).equals(new Location(50, 30)));
-    	assertFalse(b.getStartLocation(5).equals(new Location(50, 30)));
-    	assertFalse(b.getStartLocation(1).equals(new Location(50, 30)));
-    	
-    	assertNull(b.getStartLocation(10));
-    	assertNull(b.getStartLocation(6));
-    }
-    
-    @Test
-    public void textBoard_test3(){
-    	TextBoard b = new TextBoard();
-    	
-    	assertEquals(b.findLocation(new Location(6, 5), "s"), new Location(6,6));
-    	assertEquals(b.findLocation(new Location(6, 5), "n"), new Location(6,4));
-    	assertEquals(b.findLocation(new Location(6, 5), "e"), new Location(7,5));
-    	assertEquals(b.findLocation(new Location(6, 5), "w"), new Location(5,5));
-    	
-    	assertFalse(b.findLocation(new Location(6, 5), "s").equals(new Location(50, 30)));
-    	assertFalse(b.findLocation(new Location(6, 5), "n").equals(new Location(50, 30)));
-    	assertFalse(b.findLocation(new Location(45, 2), "e").equals(new Location(50, 30)));
-    }
-    
-    @Test
-    public void textBoard_test4(){
-    	TextBoard b = new TextBoard();
-    	
-    	assertTrue(b.canMoveTo(new Location(7,7)));
-    	assertFalse(b.canMoveTo(new Location(-1,-1)));
-    	assertFalse(b.canMoveTo(new Location(50,50)));
-    	assertFalse(b.canMoveTo(new Location(15, 15)));
-    	assertFalse(b.canMoveTo(new Location(0, 0)));
-    	
-    }
-    
-    @Test
-    public void textBoard_test5(){
-    	TextBoard b = new TextBoard();
-    	
-    	b.getTile(new Location(6,7)).setSuspectOn(new Suspect("Some name", new Location(6, 7)));
-    	
-    	assertTrue(b.canMoveTo(new Location(6, 6)));
-    	b.getTile(new Location(6,7)).setSuspectOn(null);
-    	assertFalse(b.canMoveTo(new Location(6, 6)));
-    	b.getTile(new Location(6,9)).setSuspectOn(new Suspect("Some name", new Location(6, 9)));
-    	
-    }
-    
-    
-    @Test
-    public void location_test1(){
-    	Location l = new Location(109238,98032457);
-    	Location l1 = new Location(5,-4);
-    	Location l2 = new Location(5, 20);
-    	Location l3 = new Location(45,12);
-    }
-    
-    @Test
-    public void location_test2(){
-    	Location l = new Location(5, 20);
-    	assertEquals(l.getX(), 5);
-    	assertEquals(l.getY(), 20);
-    	l.setX(6);
-    	l.setY(70);
-    	assertEquals(l.getX(), 6);
-    	assertEquals(l.getY(), 70);
-    	assertFalse(l.getX() == 70);
-    	assertFalse(l.getY() == 40);
-    }
-    
-    @Test
-    public void location_test3(){
-    	assertEquals(new Location(5,5), new Location(5,5));
-    	assertFalse(new Location(5,5).equals("hello"));
-    	assertFalse(new Location(5,5).equals(new Location(4,4)));
-    	assertEquals(new Location(5,5).toString(),"(5, 5)");
-    }
-    
-    @Test
-    public void intrigueTile_test1(){
-    	IntrigueTile i = new IntrigueTile(new Location(5,5), null, "intrigue", new DrawContext("?", null));
-    	assertTrue(i.canMoveTo());
-    }
-    
-    @Test
-    public void hallTile_test1(){
-    	Hall i = new Hall(new Location(5,5), null, "hall", new DrawContext(" ", null));
-    	assertTrue(i.canMoveTo());
-    }
-    
-    @Test
-    public void wall_test1(){
-    	Wall i = new Wall(new Location(5,5), null, "wall", new DrawContext("#", null));
-    	assertFalse(i.canMoveTo());
-    }
+	@Test
+	public void drawContext_text2() {
+		DrawContext dw = new DrawContext("hallway", null);
+		assertEquals(dw.getString(), "hallway");
+		assertFalse(new DrawContext("something", null).getString().equals("another thing"));
+	}
 
-    @Test
-    public void textBoard_test6(){
-    	final TextBoard b = new TextBoard();
-    	final Room r = new Room("Spa 6 6", b);
+	@Test
+	public void textBoard_test1() {
+		TextBoard b = new TextBoard();
+	}
 
-    	ArrayList<Room> a = new ArrayList<Room>(){{
-    		add(r);
-			add(new Room("Theatre 11 8", b));
-			add(new Room("Living_Room 17 9", b));
-			add(new Room("Conservatory 23 8", b));
-			add(new Room("Patio 6 12 8 13 8 17 6 18", b));
-			add(new Room("Hall 22 12 23 12 20 14 20 15", b));
-			add(new Room("Kitchen 7 23", b));
-			add(new Room("Dining_Room 13 20 16 22", b));
-			add(new Room("Guest_House 22 23 23 22", b));
-			add(new Room("Pool 15 12 18 17 11 17", b));
-    	}};
+	@Test
+	public void textBoard_test2() {
+		TextBoard b = new TextBoard();
 
-    	HashMap<Location, Room> map = b.loadRooms(a);
+		assertEquals(b.getStartLocation(0), new Location(7, 1));
+		assertEquals(b.getStartLocation(3), new Location(8, 29));
+		assertEquals(b.getStartLocation(4), new Location(1, 20));
 
-    	assertFalse(map.containsKey(new Location(7,7)));
+		assertFalse(b.getStartLocation(2).equals(new Location(50, 30)));
+		assertFalse(b.getStartLocation(5).equals(new Location(50, 30)));
+		assertFalse(b.getStartLocation(1).equals(new Location(50, 30)));
 
-    	assertTrue(map.containsValue(r));
-    }
+		assertNull(b.getStartLocation(10));
+		assertNull(b.getStartLocation(6));
+	}
+
+	@Test
+	public void textBoard_test3() {
+		TextBoard b = new TextBoard();
+
+		assertEquals(b.findLocation(new Location(6, 5), "s"), new Location(6, 6));
+		assertEquals(b.findLocation(new Location(6, 5), "n"), new Location(6, 4));
+		assertEquals(b.findLocation(new Location(6, 5), "e"), new Location(7, 5));
+		assertEquals(b.findLocation(new Location(6, 5), "w"), new Location(5, 5));
+
+		assertFalse(b.findLocation(new Location(6, 5), "s").equals(new Location(50, 30)));
+		assertFalse(b.findLocation(new Location(6, 5), "n").equals(new Location(50, 30)));
+		assertFalse(b.findLocation(new Location(45, 2), "e").equals(new Location(50, 30)));
+	}
+
+	@Test
+	public void textBoard_test4() {
+		TextBoard b = new TextBoard();
+
+		assertTrue(b.canMoveTo(new Location(7, 7)));
+		assertFalse(b.canMoveTo(new Location(-1, -1)));
+		assertFalse(b.canMoveTo(new Location(50, 50)));
+		assertFalse(b.canMoveTo(new Location(15, 15)));
+		assertFalse(b.canMoveTo(new Location(0, 0)));
+
+	}
+
+	@Test
+	public void textBoard_test5() {
+		TextBoard b = new TextBoard();
+
+		b.getTile(new Location(6, 7)).setSuspectOn(new Suspect("Some name", new Location(6, 7)));
+
+		assertTrue(b.canMoveTo(new Location(6, 6)));
+		b.getTile(new Location(6, 7)).setSuspectOn(null);
+		assertFalse(b.canMoveTo(new Location(6, 6)));
+		b.getTile(new Location(6, 9)).setSuspectOn(new Suspect("Some name", new Location(6, 9)));
+
+	}
+
+	@Test
+	public void location_test1() {
+		Location l = new Location(109238, 98032457);
+		Location l1 = new Location(5, -4);
+		Location l2 = new Location(5, 20);
+		Location l3 = new Location(45, 12);
+	}
+
+	@Test
+	public void location_test2() {
+		Location l = new Location(5, 20);
+		assertEquals(l.getX(), 5);
+		assertEquals(l.getY(), 20);
+		l.setX(6);
+		l.setY(70);
+		assertEquals(l.getX(), 6);
+		assertEquals(l.getY(), 70);
+		assertFalse(l.getX() == 70);
+		assertFalse(l.getY() == 40);
+	}
+
+	@Test
+	public void location_test3() {
+		assertEquals(new Location(5, 5), new Location(5, 5));
+		assertFalse(new Location(5, 5).equals("hello"));
+		assertFalse(new Location(5, 5).equals(new Location(4, 4)));
+		assertEquals(new Location(5, 5).toString(), "(5, 5)");
+	}
+
+	@Test
+	public void intrigueTile_test1() {
+		IntrigueTile i = new IntrigueTile(new Location(5, 5), null, "intrigue", new DrawContext("?", null));
+		assertTrue(i.canMoveTo());
+	}
+
+	@Test
+	public void hallTile_test1() {
+		Hall i = new Hall(new Location(5, 5), null, "hall", new DrawContext(" ", null));
+		assertTrue(i.canMoveTo());
+	}
+
+	@Test
+	public void wall_test1() {
+		Wall i = new Wall(new Location(5, 5), null, "wall", new DrawContext("#", null));
+		assertFalse(i.canMoveTo());
+	}
+
+	@Test
+	public void textBoard_test6() {
+		TextBoard b = new TextBoard();
+		Room r = new Room("Spa 6 6", b);
+
+		ArrayList<Room> a = new ArrayList<Room>();
+		a.add(r);
+		a.add(new Room("Theatre 11 8", b));
+		a.add(new Room("Living_Room 17 9", b));
+		a.add(new Room("Conservatory 23 8", b));
+		a.add(new Room("Patio 6 12 8 13 8 17 6 18", b));
+		a.add(new Room("Hall 22 12 23 12 20 14 20 15", b));
+		a.add(new Room("Kitchen 7 23", b));
+		a.add(new Room("Dining_Room 13 20 16 22", b));
+		a.add(new Room("Guest_House 22 23 23 22", b));
+		a.add(new Room("Pool 15 12 18 17 11 17", b));
+
+		HashMap<Location, Room> map = b.loadRooms(a);
+
+		assertFalse(map.containsKey(new Location(7, 7)));
+
+		assertTrue(map.containsValue(r));
+
+	}
 }
